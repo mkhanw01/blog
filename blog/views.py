7df7d6d76a9118c89a2e9f26e85db958146dee0f
mkhanw01@gmail.com <mkhanw01@gmail.com>
2016-08-20 15:18:31 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect, render
+from django.utils import timezone
+
+from blog.forms import PostForm
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog.post_edit.html', {'form': form})
